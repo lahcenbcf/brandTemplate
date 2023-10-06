b66481a4e840d7306e4366a00fa0cf5abb75e694
lahcenbcf <h.bencharef@esi-sba.dz>
2023-10-06 20:42:12 +0100

    add aboutSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+//import pages
+import About from './components/About/About'
 function App() {
   return (
-    <>
-      
-    </>
+    <Router>
+    <Routes>
+    <Route path='/' element={<About />} />
+        
+    </Routes>
+    </Router>
   )
 }
+
 
 export default App
---
+++ src/components/About/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import brandIdea from "../../assets/brandIdea.jpg"
+function About() {
+  return (
+    <div className='about'>
+        {/*-
+        Content
+    -*/}
+        <div className='content'>
+            <h2>We work to make</h2>
+            <h2>your <span>dreams </span></h2>
+            <h2>come true</h2>
+
+            <p>Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page, le texte définitif venant remplacer le faux-texte dès qu'il est prêt ou que la mise en page est achevée.</p>
+            <button className='learnMore'>learn more</button>
+        </div>
+
+        {/*-
+        image
+    -*/}
+
+        <img src={brandIdea} alt='brandIdea' />
+
+    </div>
+  )
+}
+
+export default About
