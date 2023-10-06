9e32132964fe45c0955e7efd8c3bb1c5fd08e497
lahcenbcf <h.bencharef@esi-sba.dz>
2023-10-06 21:28:53 +0100

    add faq section

---
+++ src/components/faq/Faq.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function Faq() {
+  return (
+    <div className='faq'>
+        <h2>Any Questions<span> FAQ</span></h2>
+        <div className='question'>
+            <h4>How Do I Start</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras</p>
+        </div>
+        <div className='question'>
+            <h4>Daily scheduels</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras</p>
+        </div>
+        <div className='question'>
+            <h4>Different payements</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras</p>
+        </div>
+        <div className='question'>
+            <h4>What Products you offer</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget sit amet tellus cras</p>
+        </div>
+    </div>
+  )
+}  
+
+export default Faq
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import About from '../components/About/About'
 import ServicesSection from '../components/services/ServicesSection'
+import Faq from '../components/faq/Faq'
 function AboutUs() {
   return (
     <div>
         <About />
         <ServicesSection />
+        <Faq />
     </div>
   )
 }
