b5151eb35fa4f0db70d5a872de4d127ad4d041c1
lahcenbcf <h.bencharef@esi-sba.dz>
2023-10-07 10:47:52 +0100

    add WorkPage

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -1,9 +1,26 @@
+import racer from "../assets/racer.jpg"
+import fitness from "../assets/fitness.jpg"
+import goodTimes from "../assets/goodTimes.jpg"
 const OurWork = () => {
   return (
-    <div>
-      Our work
+    <div className="work">
+      <div className="movie">
+          <h2>The Athelete</h2>
+          <div className="line"></div>
+          <img src={fitness} alt="athelete" />
+      </div>
+      <div className="movie">
+          <h2>Good Times</h2>
+          <div className="line"></div>
+          <img src={goodTimes} alt="good Times" className="goodTimes" />
+      </div>
+      <div className="movie">
+          <h2>The Racer</h2>
+          <div className="line"></div>
+          <img src={racer} alt="racer" />
+      </div>
     </div>
   )
 }
 
-export default OurWork
+export default OurWork 
